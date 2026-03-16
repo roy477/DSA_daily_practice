ec9dc34230d9d9c6ec9dfd8dccac863efe0500e3
resin <resinr27@gmail.com>
2026-03-16 12:26:45 +0530

    bubble sort

---
+++ day5.1.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {25, 12, 31, 23, 52, 20};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
